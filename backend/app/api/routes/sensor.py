fba1900eb0a7aea69c28228a7bdba0b361e8175a
omar-zr <omar.zr98@gmail.com>
2024-07-28 03:14:25 +0400

    minor fix

--- backend/app/api/routes/sensor.py
+++ backend/app/api/routes/sensor.py
@@ -5,6 +5,6 @@ from app.api.routes.sensorUtils.sensor_health import router as sensor_health_rou
 from app.api.routes.sensorUtils.sensor_info import router as sensor_info_router
 
 router = APIRouter()
-router.include_router(sensor_data_router, prefix="/sensordata", tags=["sensor-data"])
-router.include_router(sensor_health_router, prefix="/sensorhealth", tags=["sensor-health"])
-router.include_router(sensor_info_router, prefix="/sensorinfo", tags=["sensor-info"])
+router.include_router(sensor_data_router, prefix="/data", tags=["sensor-data"])
+router.include_router(sensor_health_router, prefix="/health", tags=["sensor-health"])
+router.include_router(sensor_info_router, prefix="/info", tags=["sensor-info"])
