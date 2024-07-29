7bf1afff7f121abc258788b6343dd9cbfc4f1d89
omar-zr <omar.zr98@gmail.com>
2024-07-29 07:05:52 +0400

    small fix

--- frontend/src/components/Generator/DataTable.tsx
+++ frontend/src/components/Generator/DataTable.tsx
@@ -1,12 +1,14 @@
 import { FC } from "react";
-import { Table, Thead, Tbody, Tr, Th, Td } from "@chakra-ui/react";
+import { Table, Thead, Tbody, Tr, Th, Td, Box } from "@chakra-ui/react";
 
 interface DataTableProps {
     tableData: any[];
 }
 
 const DataTable: FC<DataTableProps> = ({ tableData }) => {
+    tableData = [...tableData].reverse(); //To show the new one first
     return (
+        <Box overflowX="auto">
         <Table variant="simple">
             <Thead>
                 <Tr>
@@ -16,22 +18,18 @@ const DataTable: FC<DataTableProps> = ({ tableData }) => {
                     <Th>NB Motorcycle</Th>
                     <Th>NB Pedestrian</Th>
                     <Th>NB Bicycle</Th>
-                    <Th>NB Mobility Aid</Th>
                     <Th>SB Car</Th>
                     <Th>SB Motorcycle</Th>
                     <Th>SB Pedestrian</Th>
                     <Th>SB Bicycle</Th>
-                    <Th>SB Mobility Aid</Th>
                     <Th>WB Car</Th>
                     <Th>WB Motorcycle</Th>
                     <Th>WB Pedestrian</Th>
                     <Th>WB Bicycle</Th>
-                    <Th>WB Mobility Aid</Th>
                     <Th>EB Car</Th>
                     <Th>EB Motorcycle</Th>
                     <Th>EB Pedestrian</Th>
                     <Th>EB Bicycle</Th>
-                    <Th>EB Mobility Aid</Th>
                 </Tr>
             </Thead>
             <Tbody>
@@ -43,26 +41,23 @@ const DataTable: FC<DataTableProps> = ({ tableData }) => {
                         <Td>{entry.results.NB_motorcycle}</Td>
                         <Td>{entry.results.NB_pedestrian}</Td>
                         <Td>{entry.results.NB_bicycle}</Td>
-                        <Td>{entry.results.NB_mobility_aid}</Td>
                         <Td>{entry.results.SB_car}</Td>
                         <Td>{entry.results.SB_motorcycle}</Td>
                         <Td>{entry.results.SB_pedestrian}</Td>
                         <Td>{entry.results.SB_bicycle}</Td>
-                        <Td>{entry.results.SB_mobility_aid}</Td>
                         <Td>{entry.results.WB_car}</Td>
                         <Td>{entry.results.WB_motorcycle}</Td>
                         <Td>{entry.results.WB_pedestrian}</Td>
                         <Td>{entry.results.WB_bicycle}</Td>
-                        <Td>{entry.results.WB_mobility_aid}</Td>
                         <Td>{entry.results.EB_car}</Td>
                         <Td>{entry.results.EB_motorcycle}</Td>
                         <Td>{entry.results.EB_pedestrian}</Td>
                         <Td>{entry.results.EB_bicycle}</Td>
-                        <Td>{entry.results.EB_mobility_aid}</Td>
                     </Tr>
                 ))}
             </Tbody>
         </Table>
+        </Box>
     );
 };
 
